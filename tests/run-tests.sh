c1cec823ab11c547edc193e9a6a6c161a4b1d480
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-01 02:04:31 -0700

    finished run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,25 +1,18 @@
 # set -e  # Don't use this because there are lines in this script that would intentionally raise exit `<error code>` and will be caught.
 
 
-chmod +x $GITHUB_WORKSPACE/script.sh
-
 ## Test I: Non-existing dir
 $GITHUB_WORKSPACE/script.sh $GITHUB_WORKSPACE/tests/foo-dir false
-if [ $? -eq 1 ]; then
-    echo "INFO: Test I passed."
-else
-    echo "ERROR: Test I failed."
-    exit 1
-fi
+[ $? -eq 1 ] && echo "INFO: Test I passed." || (echo "ERROR: Test I failed." && exit 1)
 
 ## Test II: Recursive
 $GITHUB_WORKSPACE/script.sh $GITHUB_WORKSPACE/tests/test-dir true
-echo "INFO: Test II passed."
+[ $? -eq 0 ] && echo "INFO: Test II passed." || (echo "ERROR: Test II failed." && exit 1)
 
 ## Test III: Non-recursive
 $GITHUB_WORKSPACE/script.sh $GITHUB_WORKSPACE/tests/test-dir false
-echo "INFO: Test III passed."
+[ $? -eq 0 ] && echo "INFO: Test III passed." || (echo "ERROR: Test III failed." && exit 1)
 
 ## Test IV: No shell files found
-$GITHUB_WORKSPACE/script.sh $GITHUB_WORKSPACE/tests/test-dir false
-echo "INFO: Test IV passed."
+$GITHUB_WORKSPACE/script.sh $GITHUB_WORKSPACE/tests/test-dir-no-shell-files true
+[ $? -eq 0 ] && echo "INFO: Test IV passed." || (echo "ERROR: Test IV failed." && exit 1)
